76b607a582d7ca45833000d5e3a3cc498193aac3
김성현 <7h1320@naver.com>
2024-04-17 17:20:18 +0900

    temp

---
+++ cpp/BOJ/BOJ/2568.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNNINGS
+
+#include <iostream>
+//#include <algorithm>
+
+int dp[];
+
+
+int lower(int& arr[], int num) {
+
+	return 0;
+}
+
+
+
+int main() {
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(0); std::cout.tie(0);
+
+}
