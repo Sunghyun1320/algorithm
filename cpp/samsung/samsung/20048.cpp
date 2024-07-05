84612e3f11eb3d0442e7ec60d34cd3864ccfc718
kimsunghyun <7h1320@naver.com>
2024-07-05 16:22:50 +0900

    swea 20048 solve

--- cpp/samsung/samsung/20048.cpp
+++ cpp/samsung/samsung/20048.cpp
@@ -3,30 +3,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 
-double dp[107][107];
+int T, X, Y;
+double answer;
 
 int main() {
 	std::ios_base::sync_with_stdio(0);
 	std::cin.tie(0); std::cout.tie(0);
 	std::cout.precision(15);
-	freopen("input_20048.txt", "r", stdin);
+	//freopen("input_20048.txt", "r", stdin);
 
-	for (int i = 0; i < 106; i++) {
-		dp[i+1][0] = (((dp[i][0] + 1) * 2) + (dp[i][0] * 4 * (i<<1))) / (((i << 1) + 1) * ((i << 1) + 2));
-	}
-
-	for (int i = 0; i < 106; i++) {
-		for (int j = 0; j < 106; j++) {
-			dp[i][j + 1] = ((dp[i][j] + 1) + (dp[i][j] * (2 * i + j))) / (2 * i + j + 1);
-		}
-
-	}
-
-	int T, X, Y;
+	
 	std::cin >> T;
-	for (int testcase = 1; testcase <= T; testcase++) {
+	while (T--) {
+		answer = 0;
 		std::cin >> X >> Y;
-		std::cout << "#" << testcase << " " << dp[X][Y] << "\n" ;
+
+		for (int i = 1; i <= X; i++)
+			answer += (double)1 / (Y + 2 * i - 1);
+
+		for (int i = 1; i <= Y; i++)
+			answer += (double)1 / (i);
+
+		std::cout << answer << "\n";
+
+
 	}
 
 }
