3def9e469596e9b1f946864aa48c388265d39845
김성현 <7h1320@naver.com>
2024-07-15 20:40:37 +0900

    solving swea 19190

---
+++ cpp/samsung/samsung/19190.cpp
@@ -0,0 +1,40 @@
+#if 1
+#define _CRT_SECURE_NO_WARNNIGS
+#include <iostream>
+
+struct List {
+	int size;
+	int max_size = 5;
+	int value[5];
+
+	void push(int a) {
+		if (max_size == size) {
+
+		}
+	}
+
+
+};
+
+
+int node[14];
+int m1, m2, N;
+
+int main() {
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(0); std::cout.tie(0);
+	freopen("input_19190.txt", "r", stdin);
+
+	int T = 0;
+	int testcase = 1;
+
+	std::cin >> T;
+	while (T--) {
+
+	}
+
+}
+
+
+
+#endif
